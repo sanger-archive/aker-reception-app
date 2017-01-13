33da8fbf7f90f792c4a2c7e18a4181c66ab79219
Harriet Craven <hc6@sanger.ac.uk>
2017-01-13 14:19:38 +0000

    complete set client post generation

--- lib/set_material_client.rb
+++ lib/set_material_client.rb
@@ -3,20 +3,14 @@ require 'faraday'
 module SetMaterialClient
 
 	def self.post(params)
-		JSON.parse(get_connection.post('/api/v1/sets', { :set => params }).body)
-
-		# conn = Faraday.new(:url => get_set_url)
-		# conn.proxy Rails.application.config.set_url_default_proxy
-		# conn.headers = {'Content-Type' => 'application/vnd.api+json'} 
-		# conn = get_connection
-		# conn.post '/api/v1/sets', { :set => params }
+		conn = get_connection
+		JSON.parse(conn.post('/api/v1/sets', params.to_json).body)
 	end
 
-	def self.get
-		conn = Faraday.new(:url => get_set_url)
-		conn.proxy Rails.application.config.set_url_default_proxy
+	def self.get(uuid)
+		conn = get_connection
 		conn.headers = {'Accept' => 'application/vnd.api+json'} 
-		conn.get '/api/v1/sets'
+    	JSON.parse(get_connection.get('/api/v1/sets/'+uuid).body)
 	end
 
 	private 
