d71f7d6dc539280080d26237bd5cdca9f9c62b30
Harriet Craven <hc6@sanger.ac.uk>
2017-01-13 15:53:03 +0000

    filter hash response to init new intance of SetMaterial

--- app/models/set_material.rb
+++ app/models/set_material.rb
@@ -5,32 +5,34 @@ class SetMaterial
 
 	validates :name, presence: true	
 
-	attr_accessor :name
-
-	def attributes=(hash)
-	    hash.each do |key, value|
-	      send("name=", value["attributes"]["name"]) if respond_to?("name=")
-	    end
-	end
+	attr_accessor :id, :name
 
 	# params = {:data=>{:type=>"sets", :attributes=>{:name=>MaterialSubmission.first.id}}} 
 	def self.create_remote_set(params)
 	  	create SetMaterialClient::post(params)
 	end
 
-	# Status: 404 - there are no persisted Sets in aker-set-service
+	# Status: 402 - there are no persisted Sets in aker-set-service
 	def self.get_remote_set(params)
 		get SetMaterialClient::get(params)
 	end
 
 	def self.create(obj)
-	   filter(obj)
+	  	new filter(obj)
 	end
 
-	# from_json creates an instance of Ownership and sets the attributes
-	# attributes= method filters out none valid attributes
-	def self.filter(hash)
-	   set = SetMaterial.new
-	   set.from_json(hash.to_json)
+	private 
+	
+	def self.filter(h, k = [])
+		new_hash = {}
+		h.each_pair do |key, val|
+			if val.is_a?(Hash)
+				new_hash.merge!(filter(val))
+			else
+				new_hash[key] = val
+			end
+		end
+		new_hash.select { |k, _| ['id', 'name'].include? k}
 	end
+
 end
