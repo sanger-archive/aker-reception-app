eca471e71955a2041e9988be3ee06f98260d8f8e
Harriet Craven <hc6@sanger.ac.uk>
2017-01-13 14:19:38 +0000

    complete set client post generation

--- app/models/set_material.rb
+++ app/models/set_material.rb
@@ -1,16 +1,36 @@
 class SetMaterial
 	include SetMaterialClient
 	include ActiveModel::Model
+	include ActiveModel::Serializers::JSON
 
 	validates :name, presence: true	
 
-	# params = {:data=>{:type=>"set", :attributes=>{:name=>"string"}}} 
-	# params = {:name => 'set name'}
-	def self.create_remote_set(params)
-	  	SetMaterialClient::post(params)
+	attr_accessor :name
+
+	def attributes=(hash)
+	    hash.each do |key, value|
+	      send("name=", value["attributes"]["name"]) if respond_to?("name=")
+	    end
 	end
 
-	# def self.get_remote_set
-	# 	get
-	# end
+	# params = {:data=>{:type=>"sets", :attributes=>{:name=>MaterialSubmission.first.id}}} 
+	def self.create_remote_set(params)
+	  	create SetMaterialClient::post(params)
+	end
+
+	# Status: 404 - there are no persisted Sets in aker-set-service
+	def self.get_remote_set(params)
+		get SetMaterialClient::get(params)
+	end
+
+	def self.create(obj)
+	   filter(obj)
+	end
+
+	# from_json creates an instance of Ownership and sets the attributes
+	# attributes= method filters out none valid attributes
+	def self.filter(hash)
+	   set = SetMaterial.new
+	   set.from_json(hash.to_json)
+	end
 end
