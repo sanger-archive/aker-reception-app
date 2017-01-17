a8b64f8c6cc1defc87b3df8eff916bb5eee7db85
Harriet Craven <hc6@sanger.ac.uk>
2017-01-17 14:58:17 +0000

    updated biomaterial client to use faraday not rest-client

--- app/models/biomaterial_client.rb
+++ app/models/biomaterial_client.rb
@@ -1,33 +1,33 @@
-require 'rest-client'
+require 'faraday'
 
 module BiomaterialClient
-  def site
-	  RestClient::Resource.new(Rails.configuration.materials_service_url)
-  end
-
-  def process_response(response)
-    if response
-      JSON.parse(response.body)
-    else
-      nil
-    end    
-  end
 
   def post(data)
-	  process_response(self.site["materials"].post(data, :content_type => 'text/json'))
+    conn = get_connection
+    JSON.parse(conn.post('/materials/', data.to_json).body)
   end
 
   def put(data)
+    uuid = data[:uuid]
     data_to_send = data.reject{|k,v| k.to_sym == :uuid}
-	  process_response(self.site["materials"][data[:uuid]].put(data_to_send, :content_type => 'text/json'))
+    conn = get_connection
+    JSON.parse(conn.post('/materials/'+uuid, data_to_send.to_json).body)
   end
 
   def get(uuid)
-    begin
-    	return nil if uuid.nil?
-    	return process_response(self.site["materials"][uuid].get(:content_type => 'text/json'))
-    rescue RestClient::ExceptionWithResponse => e
-      return nil
-    end
+      return nil if uuid.nil?
+      conn = get_connection
+      conn.headers = {'Accept' => 'application/vnd.api+json'} 
+      JSON.parse(get_connection.get('/materials/'+uuid).body)
   end
+
+  private 
+  
+  def get_connection
+      conn = Faraday.new(:url => Rails.application.config.materials_service_url)
+      conn.proxy Rails.application.config.materials_service_url
+      conn.headers = {'Content-Type' => 'application/vnd.api+json'} 
+      conn
+  end
+
 end
