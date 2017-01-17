97f80cc05ccf3301b8583e53d993b8fb82e3a7b4
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-01-17 14:01:51 +0000

    Modification of factories

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,4 +40,10 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.materials_service_url = "http://localhost:5000"
+  config.set_url = 'http://localhost:1500/api/v1/sets'
+  config.set_url_default_proxy = 'http://localhost:1500'
+  config.ownership_url = 'http://localhost:4000/ownerships'
+  config.ownership_url_default_proxy = 'http://localhost:4000'
+  
+
 end
