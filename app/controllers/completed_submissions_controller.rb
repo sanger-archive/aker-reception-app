70b876da8ad54b30360acc692a5d6a20c898527c
David Robinson <dr6@sanger.ac.uk>
2017-01-24 16:44:28 +0000

    Added printer selection to completed submissions page

--- app/controllers/completed_submissions_controller.rb
+++ app/controllers/completed_submissions_controller.rb
@@ -2,21 +2,16 @@ class CompletedSubmissionsController < ApplicationController
 	def index
 		@unprinted_submissions = MaterialSubmission.active
 		@printed_submissions = MaterialSubmission.awaiting
+		@printers = Printer.all
 	end
 
 	def print
-		completed_submission_ids = params[:completed_submission_ids]
-		unless completed_submission_ids
-			return print_error "Please specify submissions to print."
-		end
-		completed_submissions = MaterialSubmission.find(completed_submission_ids.map { |s| s.to_i })
-		unless completed_submissions.all? { |ms| ms.active_or_awaiting? }
-			return print_error "Cannot print incomplete submissions."
-		end
+		printparams = print_params
 
-		# Do print somehow
-		printername = "d304bc"
-		if not Printer.find_by(name: printername).print_submissions(completed_submissions)
+		completed_submissions = MaterialSubmission.find(printparams[:completed_submission_ids])
+		printer = Printer.find_by(name: printparams[:printer_name])
+
+		unless printer.print_submissions(completed_submissions)
 			return print_error "Printing failed."
 		end
 
@@ -27,6 +22,12 @@ class CompletedSubmissionsController < ApplicationController
 	end
 
 private
+	def print_params
+		{
+			completed_submission_ids: params.require(:completed_submission_ids).map { |s| s.to_i },
+			printer_name: params.require(:printer).require(:name)
+		}
+	end
     def print_error(message)
     	redirect_back fallback_location: completed_submissions_url, flash: { error: message }
     end
