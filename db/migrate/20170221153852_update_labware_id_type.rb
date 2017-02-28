6b9d219c251c9b9a058dfb7255c0f61320be2259
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-02-28 13:57:46 +0000

    Change labwre to labwares

--- db/migrate/20170221153852_update_labware_id_type.rb
+++ db/migrate/20170221153852_update_labware_id_type.rb
@@ -1,7 +1,7 @@
 class UpdateLabwareIdType < ActiveRecord::Migration[5.0]
   def change
     ActiveRecord::Base.transaction do
-      remove_foreign_key :material_submission_labwares, :labware
+      remove_foreign_key :material_submission_labwares, :labwares
       change_column :material_submission_labwares, :labware_id, :string
     end
   end
