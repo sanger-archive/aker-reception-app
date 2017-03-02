9e454801d98f697b08010b55531a9f3e8d10565a
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-03-02 11:52:26 +0000

    Modified emails to receive the submission id in both the external and internal contact

--- app/mailers/material_submission_mailer.rb
+++ app/mailers/material_submission_mailer.rb
@@ -3,12 +3,12 @@ class MaterialSubmissionMailer < ApplicationMailer
   def submission_confirmation(material_submission)
     @material_submission = material_submission
     @contact_fullname = material_submission.contact.fullname
-    mail(to: material_submission.email, subject: "Material Submission Confirmation")
+    mail(to: material_submission.email, subject: "Material Submission #{@material_submission.id} confirmation")
   end
 
   def notify_contact(material_submission)
     @material_submission = material_submission
     @collaborator_email = material_submission.email
-    mail(to: material_submission.contact.email, subject: "New Material Submission Created")
+    mail(to: material_submission.contact.email, subject: "New Material Submission #{@material_submission.id} created")
   end
 end
