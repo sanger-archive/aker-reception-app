dd9bc60724b0a4dbdeb7054dea79f69cca2cea9d
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-04-18 16:59:44 +0100

    Removed and modifying of out of date test

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -10,15 +10,15 @@ RSpec.describe Users::SessionsController, type: :controller do
       groups = ["cowboys"]
 
       user = create(:user)
-      expect(user).to receive(:fetch_groups).and_return(groups)
+      expect(user).to receive(:groups).and_return(groups)
       allow(request.env['warden']).to receive(:authenticate!).and_return(user)
       allow(controller).to receive(:current_user).and_return(user)
       post :create, params: { user: { email: "jeff", password: 'abc123' } }
       expect(response).to redirect_to(root_url)
       userinfo = session["user"]
 
-      expect(userinfo["user"]["email"]).to eq user.email
-      expect(userinfo["groups"]).to eq groups
+      expect(userinfo.email).to eq user.email
+      expect(userinfo.groups).to eq groups
     end
 
   end
