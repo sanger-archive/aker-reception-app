1a04b044c55aa4475a41a72e77e54fc7eeebbada
Harriet Craven <hc6@sanger.ac.uk>
2017-05-05 17:00:07 +0100

    added claim_submissions_controller spec

--- app/controllers/claim_submissions_controller.rb
+++ app/controllers/claim_submissions_controller.rb
@@ -10,15 +10,6 @@ class ClaimSubmissionsController < ApplicationController
     end
   end
 
-  def find_submissions_by_user
-    if @contact.nil? || @submissions.empty?
-      @json = {:error => 'No submissions found'}
-    else
-      @json = @submissions.to_json
-    end
-    render json: @json
-  end
-
   def get_all_collections
     collection_uuids = StudyClient::Collection.all.map { |n| n.set_id }
     render json: SetClient::Set.get_set_names(collection_uuids).to_json
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+require 'rails/commands/server'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
