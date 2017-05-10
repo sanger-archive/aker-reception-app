eea9f858f4d215ea1d7175833b2f7e1122b11055
Harriet Craven <hc6@sanger.ac.uk>
2017-05-10 15:05:39 +0100

    Added a test for deleting a material_submission deletes its labwares

---
+++ spec/controllers/material_submissions_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe MaterialSubmissionsController, type: :controller do
+
+  describe "#destory" do
+
+    setup do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+
+      @user = FactoryGirl.create(:user)
+      @labware_type = FactoryGirl.create :labware_type, {
+        :num_of_cols => 1,
+        :num_of_rows => 1
+      }
+      sign_in(@user)
+    end
+
+
+    it 'deletes the material submission and labwares' do
+      @material_uuid = SecureRandom.uuid
+      @material_submission = FactoryGirl.create(:material_submission, user: @user)
+      @labware = Labware.create(material_submission: @material_submission, labware_index: 1, barcode: "AKER-1", print_count: 1, contents: {"1": { id: "#{@material_uuid}" } })
+      @material_submission.labwares << @labware
+
+      expect{ delete :destroy, :id => @material_submission.id}
+      .to change(MaterialSubmission, :count).by(-1)
+      .and change(Labware, :count).by(-1)
+    end
+
+  end
+end
