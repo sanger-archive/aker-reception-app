324ce9251a4c3e855c0b0baab3cf49adde156e43
David Robinson <dr6@sanger.ac.uk>
2017-06-21 16:46:10 +0100

    a100 in migration, update already claimed submissions and labware

--- db/migrate/20170621134513_add_claimed_to_labware.rb
+++ db/migrate/20170621134513_add_claimed_to_labware.rb
@@ -1,5 +1,11 @@
 class AddClaimedToLabware < ActiveRecord::Migration[5.0]
   def change
     add_column :labwares, :claimed, :timestamp
+
+    claimed_submissions = MaterialSubmission.where(status: 'claimed')
+    claimed_submissions.flat_map(&:labwares).each do |lw|
+      lw.update_attributes(claimed: DateTime.now)
+    end
+    claimed_submissions.update_all(status: MaterialSubmission.AWAITING)
   end
 end
