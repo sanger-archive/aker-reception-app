f90bfc51160c5cbf8665c837ede38b622e68ede1
David Robinson <dr6@sanger.ac.uk>
2017-06-23 09:47:58 +0100

    (experimental) - unset proxies when using study client

--- config/initializers/study_client.rb
+++ config/initializers/study_client.rb
@@ -4,6 +4,10 @@ Rails.application.config.after_initialize do
   StudyClient::Base.site = Rails.application.config.study_url
 
   StudyClient::Base.connection do |connection|
+    ENV['HTTP_PROXY'] = nil
+    ENV['http_proxy'] = nil
+    ENV['https_proxy'] = nil
+    connection.faraday.proxy ''
     connection.use JWTSerializer
   end
 
