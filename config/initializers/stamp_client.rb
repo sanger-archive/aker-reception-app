72db652f2181ae98f35fceb6833497f2eabe9238
Philip Jansen van Vuuren <pj5@sanger.ac.uk>
2017-10-03 16:43:30 +0100

    Update client initializers to remove deprecation warning

--- config/initializers/stamp_client.rb
+++ config/initializers/stamp_client.rb
@@ -1,15 +1,18 @@
 Rails.application.config.after_initialize do
-
   StampClient::Base.site = Rails.application.config.stamp_url
 
   StampClient::Base.connection do |connection|
     ENV['HTTP_PROXY'] = nil
     ENV['http_proxy'] = nil
     ENV['https_proxy'] = nil
-    connection.faraday.proxy ''
+
+    # Remove deprecation warning by sending empty hash
+    # http://www.rubydoc.info/github/lostisland/faraday/Faraday/Connection
+    connection.faraday.proxy {}
     connection.use JWTSerializer
+
     if Rails.env.production? || Rails.env.staging?
-      connection.use ZipkinTracer::FaradayHandler, 'Stamp service'
+      connection.use ZipkinTracer::FaradayHandler, "Stamp service"
     end
   end
 end
