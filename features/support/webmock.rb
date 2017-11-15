a3b8f48a9c56382022882b33d37c7aa70cb11bc5
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-11-15 14:25:12 +0000

    Allow connections to Google Fonts

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,3 +1,2 @@
 require 'webmock/cucumber'
-WebMock.disable_net_connect!(allow_localhost: true)
-WebMock.disable_net_connect!(allow: 'fonts.googleapis.com')
+WebMock.disable_net_connect!(allow: %r{__identify__|fonts.googleapis.com})
