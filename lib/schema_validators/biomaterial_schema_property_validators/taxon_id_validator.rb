7832deaf592cd97a725976f248023714735d2c55
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-11-20 12:15:40 +0000

    Caches the value of tax id for the lifetime of the validator (that is the lifetime of the current validation process)

--- lib/schema_validators/biomaterial_schema_property_validators/taxon_id_validator.rb
+++ lib/schema_validators/biomaterial_schema_property_validators/taxon_id_validator.rb
@@ -1,8 +1,20 @@
 module SchemaValidators
   module BiomaterialSchemaPropertyValidators
     class TaxonIdValidator < BiomaterialSchemaPropertyValidator
+      attr_reader :taxonomies_memoized
+
       def self.is_applicable?(property_name, property_data)
         property_name == 'tax_id'
+      end
+
+      def find_by_tax_id(tax_id)
+        if taxonomies_memoized.nil?
+          taxonomies_memoized = {}
+        end
+        if !taxonomies_memoized[tax_id]
+          taxonomies_memoized[tax_id] = TaxonomyClient::Taxonomy.find(tax_id)
+        end
+        taxonomies_memoized[tax_id]
       end
 
       def validate(labware_index, address, bio_data)
@@ -11,7 +23,7 @@ module SchemaValidators
         tax_id = field_data_for_property('tax_id', bio_data)
         scientific_name = field_data_for_property('scientific_name', bio_data)
         begin
-          obtained_value = TaxonomyClient::Taxonomy.find(tax_id).scientificName
+          obtained_value = find_by_tax_id(tax_id).scientificName
         rescue TaxonomyClient::Errors::NotFound => e
           add_error(labware_index, address, property_name, "The Tax Id provided (#{tax_id}) was not found in the EBI Taxonomy service")
           return false          
