4ce6217d104c05923265a150f2f1133a76158378
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-11-29 11:41:38 +0000

    Added warning when the manifest contains a scientific name, as it is overwritten by taxon service Added warning mark to each input that has a changed scientific name after loading a manifest

--- app/assets/javascripts/taxonomy_id_control.js
+++ app/assets/javascripts/taxonomy_id_control.js
@@ -14,13 +14,16 @@
     window._taxonomyCache = $.extend(window._taxonomyCache, params.cachedTaxonomies);
     this._cacheStorage = window._taxonomyCache;
 
+    this.numAjaxCalls = 0;
+    this.lastStoredCall = (-1);
+
     this.attachHandlers();
   };
 
   var proto = TaxonomyIdControl.prototype;
 
   proto.attachHandlers = function() {
-    $(this.node).closest('table').on('psd.update-table', $.proxy(this.findTaxId, this, true));
+    $(this.node).closest('table').on('psd.update-table', $.proxy(this.onUpdateTable, this));
     this.inputTaxId.on('keyup', $.proxy(this.findTaxId, this, false));
     this.inputTaxId.on('change', $.proxy(this.findTaxId, this, false));
     this.inputTaxId.on('blur', $.proxy(this.findTaxId, this, false));
@@ -30,6 +33,43 @@
     // is not editable
     this.inputSciName.attr('readonly', true);
     this.inputSciName.on('focus', $.proxy(this.focusToNextInput, this, this.inputSciName));
+  };
+
+  proto.showWarning = function() {
+    var warningAlert = $('#warning-messages');
+    warningAlert.html(
+        ["<div>The manifest should not provide a scientific name, as this value is obtained from the taxonomy id.</div>",
+        "<div><b><u>We will ignore the value of the scientific name from the manifest and will use the corresponding for the taxonomy id provided.</u></b> </div>", 
+        "<div>Please review the value of the scientific name in this view. If this is not what you expected please review the contents of the manifest and provide the appropiate ",
+        "taxonomy id</div>"].join('')
+    );
+    warningAlert.toggleClass('hidden', false);
+  };
+
+  proto.hideWarning = function() {
+    var warningAlert = $('#warning-messages');
+    warningAlert.toggleClass('hidden', true);
+  };
+
+  proto.manifestWithScientificNameWarning = function() {
+    this.hideWarning();
+    if (this.getScientificName().length > 0) {
+      this.showWarning();
+    }
+  };
+
+  proto.manifestWithDifferentValueForScientificNameInputWarning = function() {
+    var previousValue = this.getScientificName();    
+    this.findTaxId(true);
+    var actualValue = this.getScientificName();
+    if ((previousValue.length > 0) && (previousValue != actualValue)) {
+      this.markInputsAs('has-warning');
+    }
+  };
+
+  proto.onUpdateTable = function() {
+    this.manifestWithScientificNameWarning();
+    this.manifestWithDifferentValueForScientificNameInputWarning();
   };
 
   proto.focusToNextInput = function(input) {
@@ -71,6 +111,27 @@
     this.inputSciName.attr('title', scientificName);
   };
 
+  proto.getScientificName = function() {
+    return this.inputSciName.val();
+  };
+
+  proto.getTaxonId = function() {
+    return this.inputTaxId.val();
+  };
+
+  /**
+  * There is a chance that the response of the Ajax calls come back in a different order to the one they were
+  * emited to the server, leaving with an inconsistent value from what the user typed. This method ensures that
+  * we will ignore any ajax response that is older than the last one we have used to update the interface.
+  **/
+  proto.executeHandlerOnlyIfNewerCall = function(handler, numCall) {
+    var restArguments = Array.prototype.splice.call(arguments, 2);
+    if (numCall > this.lastStoredCall) {
+      this.lastStoredCall = numCall;
+      return handler.apply(this, restArguments);
+    }
+  };
+
   proto.onSuccessFindTaxId = function(data) {
     this._cacheStorage[data.taxId] = data;
     this.setScientificName(data.scientificName);
@@ -82,8 +143,8 @@
   };
 
   proto.findTaxId = function(synchronous) {
-    var taxId = this.inputTaxId.val();
-    this.inputSciName.attr('value', '');    
+    var taxId = this.getTaxonId();
+    this.setScientificName('');    
     if (this.validateTaxId(taxId)) {
       if (taxId.length == 0) {
         this.toggleMark('has-success', false);
@@ -93,11 +154,13 @@
       if (typeof this._cacheStorage[taxId] !== 'undefined') {
         return this.onSuccessFindTaxId(this._cacheStorage[taxId]);
       }
+
+      this.numAjaxCalls += 1;
       $.ajax({
         url: this.taxonomyServiceUrl+'/'+taxId,
         method: 'GET',
-        success: $.proxy(this.onSuccessFindTaxId, this),
-        error: $.proxy(this.onErrorFindTaxId, this),
+        success: $.proxy(this.executeHandlerOnlyIfNewerCall, this, this.onSuccessFindTaxId, this.numAjaxCalls),
+        error: $.proxy(this.executeHandlerOnlyIfNewerCall, this, this.onErrorFindTaxId, this.numAjaxCalls),
         async: !synchronous
       });      
     } else {
