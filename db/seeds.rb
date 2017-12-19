9c7d95fc5de359d21d3d4a53fbc654e06ef5b47c
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-12-19 12:01:11 +0000

    Remove old groups from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -125,7 +125,7 @@ printers.each do |args|
   end
 end
 
-['akerug', 'sccp', 'psd_group', 'psg', 'cgp_transfer'].each do |name|
+['akerug', 'psg'].each do |name|
   unless ContactGroup.find_by(name: name)
     ContactGroup.create(name: name)
   end
