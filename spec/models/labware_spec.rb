12784c8e2daa82c071a48f6a9b95e16ee1773891
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-12-20 11:51:02 +0000

    Fix labware test

--- spec/models/labware_spec.rb
+++ spec/models/labware_spec.rb
@@ -59,7 +59,7 @@ RSpec.describe Labware, type: :model do
       end
 
       it 'returns an array with letters for columns' do
-        expected = '1:A 1:B 2:A 2:B 3:A 3:B'.split
+        expected = '1:A 2:A 3:A 1:B 2:B 3:B'.split
         expect(@labware.positions).to eq(expected)
       end
     end
