83f74891cb8a755f962346a3f13545f010bd4e46
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-12-20 11:37:45 +0000

    Sort material submissions by column, instead of row

--- spec/models/labware_spec.rb
+++ spec/models/labware_spec.rb
@@ -70,7 +70,7 @@ RSpec.describe Labware, type: :model do
       end
 
       it 'returns an array with letters for rows' do
-        expected = 'A:1 A:2 B:1 B:2 C:1 C:2'.split
+        expected = 'A:1 B:1 C:1 A:2 B:2 C:2'.split
         expect(@labware.positions).to eq(expected)
       end
     end
