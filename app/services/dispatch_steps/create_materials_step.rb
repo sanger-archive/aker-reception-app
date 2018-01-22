de110d8385391b7ac37086d4a447b802a05f87df
Philip Jansen van Vuuren <pj5@sanger.ac.uk>
2018-01-22 16:03:45 +0000

    Perform some linting

--- app/services/dispatch_steps/create_materials_step.rb
+++ app/services/dispatch_steps/create_materials_step.rb
@@ -1,44 +1,41 @@
-# Step to create materials via MatconClient
-module DispatchSteps
-  class CreateMaterialsStep
+# frozen_string_literal: true
 
+module DispatchSteps
+  # Step to create materials via MatconClient
+  class CreateMaterialsStep
     def initialize(material_submission)
       @material_submission = material_submission
     end
 
     # If you're trying to be safe, you need to make sure errors from this method are caught.
     def up
-      @material_submission.labwares.each do | labware |
+      @material_submission.labwares.each do |labware|
         changed = false
         contents = labware.contents
-        contents.each do | address, bio_data |
-          unless bio_data['id']
-            m = MatconClient::Material.create(bio_data.merge(owner_id: @material_submission.contact.email))
-            bio_data['id'] = m.id
-            changed = true
-          end
+        contents.each_value do |bio_data|
+          next if bio_data['id']
+          m = MatconClient::Material.create(bio_data.merge(
+                                              owner_id: @material_submission.contact.email
+          ))
+          bio_data['id'] = m.id
+          changed = true
         end
-        if changed
-          labware.update_attributes(contents: contents)
-        end
+        changed && labware.update_attributes(contents: contents)
       end
     end
 
     # If you're trying to be safe, you need to make sure errors from this method are caught.
     def down
-      @material_submission.labwares.each do | labware |
+      @material_submission.labwares.each do |labware|
         changed = false
         contents = labware.contents
-        contents.each do | address, bio_data |
-          if bio_data['id']
-            MatconClient::Material.destroy(bio_data['id'])
-            bio_data.delete('id')
-            changed = true
-          end
+        contents.each_value do |bio_data|
+          next unless bio_data['id']
+          MatconClient::Material.destroy(bio_data['id'])
+          bio_data.delete('id')
+          changed = true
         end
-        if changed
-          labware.update_attributes(contents: contents)
-        end
+        changed && labware.update_attributes(contents: contents)
       end
     end
   end
