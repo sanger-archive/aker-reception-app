71635e2f371e8fab0770e7d7d4c0514ebe101a1f
Chris Smith <cs24@sanger.ac.uk>
2018-01-23 14:04:26 +0000

    Show fewer error boxes for provenance validation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,12 +63,6 @@ Rails.application.configure do
   config.set_url = 'http://localhost:3000/api/v1/'
   config.set_url_default_proxy = 'http://localhost:3000'
 
-  config.study_url = 'http://localhost:3300/api/v1/'
-  config.study_url_default_proxy = 'http://localhost:3300'
-
-  config.ownership_url = 'http://localhost:4000/ownerships'
-  config.ownership_url_default_proxy = 'http://localhost:4000'
-
   config.stamp_url = 'http://localhost:7000/api/v1/'
 
   config.ehmdmc_url = 'http://localhost:3501/validate'
