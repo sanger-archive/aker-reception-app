26be02d664fe3e440c9f4ece3075841bcfa22e92
Alex Cummins <alex.cummins@sanger.ac.uk>
2018-04-25 16:58:39 +0100

    Disable proxy change for taxon client

--- config/initializers/taxonomy_client.rb
+++ config/initializers/taxonomy_client.rb
@@ -7,13 +7,12 @@ Rails.application.config.after_initialize do
     # Remove deprecation warning by sending empty hash
     # http://www.rubydoc.info/github/lostisland/faraday/Faraday/Connection
 
-
     # Enable HTTP cache
     connection.use Faraday::HttpCache, store: Rails.cache
   end
 
   # Use Sanger proxy everywhere except local 'development'
-  if !Rails.env.development?
-    TaxonomyClient::Taxonomy.connection.faraday.proxy = Rails.configuration.aker_deployment_default_proxy
-  end
+  # if !Rails.env.development?
+  #   TaxonomyClient::Taxonomy.connection.faraday.proxy = Rails.configuration.aker_deployment_default_proxy
+  # end
 end
