c3c37520147cf9114b0cef2c7967ddbfe07f1754
Alex Cummins <alex.cummins@sanger.ac.uk>
2018-04-26 09:35:04 +0100

    Don't use Sanger proxy on OpenStack WIP environment

--- config/initializers/taxonomy_client.rb
+++ config/initializers/taxonomy_client.rb
@@ -11,8 +11,8 @@ Rails.application.config.after_initialize do
     connection.use Faraday::HttpCache, store: Rails.cache
   end
 
-  # Use Sanger proxy everywhere except local 'development'
-  # if !Rails.env.development?
-  #   TaxonomyClient::Taxonomy.connection.faraday.proxy = Rails.configuration.aker_deployment_default_proxy
-  # end
+  # Use Sanger proxy everywhere except local 'development' and new OpenStack 'wip' environment
+  if !(Rails.env.development? || !Rails.env.wip?)
+    TaxonomyClient::Taxonomy.connection.faraday.proxy = Rails.configuration.aker_deployment_default_proxy
+  end
 end
