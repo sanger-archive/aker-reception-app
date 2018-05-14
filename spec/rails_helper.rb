ccc7693d3e1354eecba86afe49e5cc6cc71a3b93
Eduardo Martin Rojo <emr@sanger.ac.uk>
2018-05-14 15:41:05 +0100

    Chrome headless, another try

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -96,12 +96,20 @@ Capybara.register_driver :poltergeist_debug do |app|
 end
 
 Capybara.register_driver :chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new(
-    args: %w[headless disable-gpu no-sandbox]
-  )
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
-Capybara.javascript_driver = :chrome
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :headless_chrome
+
 
 WebMock.disable_net_connect!(:allow_localhost => true)
