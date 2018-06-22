7b577e83d75d70dc26df5b7ae8062f9b2e1f215e
Philip Jansen van Vuuren <pj5@sanger.ac.uk>
2018-06-22 13:25:57 +0100

    Schema updated after running migrate

--- db/schema.rb
+++ db/schema.rb
@@ -16,90 +16,90 @@ ActiveRecord::Schema.define(version: 20180315143014) do
   enable_extension "plpgsql"
   enable_extension "citext"
 
-  create_table "contact_groups", force: :cascade do |t|
-    t.citext   "name",       null: false
+  create_table "contact_groups", id: :serial, force: :cascade do |t|
+    t.citext "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_contact_groups_on_name", unique: true, using: :btree
+    t.index ["name"], name: "index_contact_groups_on_name", unique: true
   end
 
-  create_table "contacts", force: :cascade do |t|
-    t.citext   "fullname",   null: false
-    t.citext   "email",      null: false
+  create_table "contacts", id: :serial, force: :cascade do |t|
+    t.citext "fullname", null: false
+    t.citext "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_contacts_on_email", unique: true, using: :btree
+    t.index ["email"], name: "index_contacts_on_email", unique: true
   end
 
-  create_table "label_templates", force: :cascade do |t|
-    t.string   "name",          null: false
-    t.string   "template_type"
-    t.integer  "external_id",   null: false
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["name"], name: "index_label_templates_on_name", unique: true, using: :btree
-  end
-
-  create_table "labware_types", force: :cascade do |t|
-    t.integer  "num_of_cols"
-    t.integer  "num_of_rows"
-    t.boolean  "col_is_alpha"
-    t.boolean  "row_is_alpha"
-    t.citext   "name",                          null: false
-    t.string   "description"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.boolean  "uses_decapper", default: false, null: false
-    t.index ["name"], name: "index_labware_types_on_name", unique: true, using: :btree
-  end
-
-  create_table "labwares", force: :cascade do |t|
-    t.integer  "material_submission_id",             null: false
-    t.integer  "labware_index",                      null: false
-    t.integer  "print_count",            default: 0, null: false
-    t.json     "contents"
-    t.string   "barcode"
-    t.string   "container_id"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.index ["barcode"], name: "index_labwares_on_barcode", unique: true, using: :btree
-    t.index ["container_id"], name: "index_labwares_on_container_id", unique: true, using: :btree
-    t.index ["material_submission_id", "labware_index"], name: "index_labwares_on_material_submission_id_and_labware_index", unique: true, using: :btree
-  end
-
-  create_table "material_receptions", force: :cascade do |t|
+  create_table "label_templates", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "template_type"
+    t.integer "external_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "labware_id", null: false
-    t.index ["labware_id"], name: "index_material_receptions_on_labware_id", unique: true, using: :btree
+    t.index ["name"], name: "index_label_templates_on_name", unique: true
   end
 
-  create_table "material_submissions", force: :cascade do |t|
-    t.integer  "no_of_labwares_required"
-    t.boolean  "supply_labwares"
-    t.integer  "labware_type_id"
-    t.string   "status"
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
-    t.text     "address"
-    t.integer  "contact_id"
-    t.uuid     "set_id"
-    t.string   "material_submission_uuid"
-    t.citext   "owner_email"
-    t.boolean  "dispatched",               default: false
-    t.boolean  "supply_decappers",         default: false, null: false
+  create_table "labware_types", id: :serial, force: :cascade do |t|
+    t.integer "num_of_cols"
+    t.integer "num_of_rows"
+    t.boolean "col_is_alpha"
+    t.boolean "row_is_alpha"
+    t.citext "name", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "uses_decapper", default: false, null: false
+    t.index ["name"], name: "index_labware_types_on_name", unique: true
+  end
+
+  create_table "labwares", id: :serial, force: :cascade do |t|
+    t.integer "material_submission_id", null: false
+    t.integer "labware_index", null: false
+    t.integer "print_count", default: 0, null: false
+    t.json "contents"
+    t.string "barcode"
+    t.string "container_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["barcode"], name: "index_labwares_on_barcode", unique: true
+    t.index ["container_id"], name: "index_labwares_on_container_id", unique: true
+    t.index ["material_submission_id", "labware_index"], name: "index_labwares_on_material_submission_id_and_labware_index", unique: true
+  end
+
+  create_table "material_receptions", id: :serial, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "labware_id", null: false
+    t.index ["labware_id"], name: "index_material_receptions_on_labware_id", unique: true
+  end
+
+  create_table "material_submissions", id: :serial, force: :cascade do |t|
+    t.integer "no_of_labwares_required"
+    t.boolean "supply_labwares"
+    t.integer "labware_type_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "address"
+    t.integer "contact_id"
+    t.uuid "set_id"
+    t.string "material_submission_uuid"
+    t.citext "owner_email"
+    t.boolean "dispatched", default: false
+    t.boolean "supply_decappers", default: false, null: false
     t.datetime "dispatch_date"
-    t.index ["contact_id"], name: "index_material_submissions_on_contact_id", using: :btree
-    t.index ["labware_type_id"], name: "index_material_submissions_on_labware_type_id", using: :btree
-    t.index ["owner_email"], name: "index_material_submissions_on_owner_email", using: :btree
+    t.index ["contact_id"], name: "index_material_submissions_on_contact_id"
+    t.index ["labware_type_id"], name: "index_material_submissions_on_labware_type_id"
+    t.index ["owner_email"], name: "index_material_submissions_on_owner_email"
   end
 
-  create_table "printers", force: :cascade do |t|
-    t.citext   "name",       null: false
-    t.string   "label_type"
+  create_table "printers", id: :serial, force: :cascade do |t|
+    t.citext "name", null: false
+    t.string "label_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_printers_on_name", unique: true, using: :btree
+    t.index ["name"], name: "index_printers_on_name", unique: true
   end
 
   add_foreign_key "labwares", "material_submissions", on_delete: :cascade
