503faee910a35c3b12c899262c076a63e5093101
Alex Cummins <alex.cummins@sanger.ac.uk>
2018-06-29 14:51:40 +0100

    Update HMDMC page

--- app/helpers/submission_helper.rb
+++ app/helpers/submission_helper.rb
@@ -7,16 +7,6 @@ module SubmissionHelper
     LabwareType.all.map{|c| ["#{c.name} (#{c.description})", c.id]}
   end
 
-  def hmdmc_info
-    "<p>Commercially available human cell lines are defined as "\
-    "human cells that have been cultured and have divided "\
-    "outside the body, and have been purchased from a "\
-    "recognised commercial supplier.</p>\n"\
-    "<p>This definition does not include, for example, "\
-    "cell lines obtained from a collaborating academic "\
-    "institution.</p>".html_safe
-  end
-
   def step_classes(step_index)
     if params[:id] == wizard_steps[step_index].to_s
       "active"
