15ef343b79f80e524f3f7502976131af76adae58
Chris Smith <cs24@sanger.ac.uk>
2018-10-23 16:43:29 +0100

    Show loading overlay when uploading a manifest

--- spec/features/show_printed_manifests_spec.rb
+++ spec/features/show_printed_manifests_spec.rb
@@ -6,6 +6,8 @@ RSpec.feature "ShowPrintedManifests", type: :feature, js: true do
     login
   end
 
+  let!(:printed_manifests) { create_list(:printed_manifest, 3) }
+
   let(:visit_printed_manifests) do
     visit manifests_print_index_path
     click_button "View Previously Printed Manifests"
@@ -13,25 +15,22 @@ RSpec.feature "ShowPrintedManifests", type: :feature, js: true do
 
   describe '#index' do
 
-    it 'displays the "Dispatch Labware" title' do
+    before(:each) do
       visit_printed_manifests
+    end
+
+    it 'displays the "Dispatch Labware" title' do
       expect(page).to have_text("Dispatch Labware")
     end
 
     it 'displays some helpful text' do
-      visit_printed_manifests
       expect(page).to have_text("These Manifests have had labels printed")
     end
 
     context 'when there are previously printed Manifests' do
 
-      before do
-        @printed_manifests = create_list(:printed_manifest, 3)
-        visit_printed_manifests
-      end
-
       it 'displays Manifests that have been printed' do
-        @printed_manifests.each do |manifest|
+        printed_manifests.each do |manifest|
           expect(page).to have_css('td', text: manifest.id)
         end
       end
