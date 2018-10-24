9e385082785152b91d6966a117f33a5947538f93
Chris Smith <cs24@sanger.ac.uk>
2018-10-24 14:37:35 +0100

    More refactoring to try and get these specs to pass

--- spec/features/show_dispatched_manifests_spec.rb
+++ spec/features/show_dispatched_manifests_spec.rb
@@ -6,6 +6,8 @@ RSpec.feature "ShowDispatchedManifests", type: :feature, js: true do
     login
   end
 
+  let!(:dispatched_manifests) { create_list(:dispatched_manifest, 3) }
+
   let(:visit_dispatched_manifests) do
     visit manifests_dispatch_index_path
     click_button "View Previously Dispatched Manifests"
@@ -13,25 +15,22 @@ RSpec.feature "ShowDispatchedManifests", type: :feature, js: true do
 
   describe '#index' do
 
-    it 'displays the "Dispatch Labware" title' do
+    before(:each) do
       visit_dispatched_manifests
+    end
+
+    it 'displays the "Dispatch Labware" title' do
       expect(page).to have_text("Dispatch Labware")
     end
 
     it 'displays some helpful text' do
-      visit_dispatched_manifests
-      expect(page).to have_text("These Manifests have been dispatched", wait: 3)
+      expect(page).to have_text("These Manifests have been dispatched", wait: 5)
     end
 
     context 'when there are previously dispatched Manifests' do
 
-      before do
-        @dispatched_manifests = create_list(:dispatched_manifest, 3)
-        visit_dispatched_manifests
-      end
-
       it 'displays Manifests that have been dispatched' do
-        @dispatched_manifests.each do |manifest|
+        dispatched_manifests.each do |manifest|
           expect(page).to have_css('td', text: manifest.id)
         end
       end
